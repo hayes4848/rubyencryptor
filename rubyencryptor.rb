ee92d3f16e8436973cdd3727852447c71b497aeb
Andy Hayes <andythemanhayes@hotmail.com>
2014-01-23 16:31:00 -0700

    changes to cipher

--- rubyencryptor.rb
+++ rubyencryptor.rb
@@ -1,5 +1,5 @@
 class Encryptor
-	def cipher
+	#def cipher
 	{"a" => "n","b" => "o","c" => "p","d" => "q",
 	 "e" => "r","f" => "s","g" => "t","h" => "u",
 	 "i" => "v","j" => "w","k" => "x","l" => "y",
@@ -9,12 +9,12 @@ class Encryptor
 	 "y" => "l","z" => "m"," " => "*"}
 	end
 	
-	def encrypt_letter(letter)
+	def encrypt_letter(letter, rotation)
 		lowercase_letter = letter.downcase
 		cipher[lowercase_letter]
 	end
 
-	def encrypt(string)
+	def encrypt(string, rotation)
 		#1 cut the input string into letters
 		letters = string.split("")
 	#2Encrypt those letters one at a time
@@ -27,6 +27,13 @@ class Encryptor
 	#3join the results back together
 		encrypted_letters.join("")
 	end
+
+	def cipher(rotation)
+  characters = (' '..'z').to_a
+  rotated_characters = characters.rotate(rotation)
+  Hash[characters.zip(rotated_characters)]
+end
+
 
 end
 
