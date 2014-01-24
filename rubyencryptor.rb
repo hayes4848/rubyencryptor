f9a2dff01aaf5e359e1b27f003d5cce158d1dadb
Andy Hayes <andythemanhayes@hotmail.com>
2014-01-23 18:21:52 -0700

    added encrypt file method

--- rubyencryptor.rb
+++ rubyencryptor.rb
@@ -1,39 +1,60 @@
 class Encryptor
 	#def cipher
-	{"a" => "n","b" => "o","c" => "p","d" => "q",
-	 "e" => "r","f" => "s","g" => "t","h" => "u",
-	 "i" => "v","j" => "w","k" => "x","l" => "y",
-	 "m" => "z","n" => "a","o" => "b","p" => "c",
-	 "q" => "d","r" => "e","s" => "f","t" => "g",
-	 "u" => "h","v" => "i","w" => "j","x" => "k",
-	 "y" => "l","z" => "m"," " => "*"}
-	end
+	# {"a" => "n","b" => "o","c" => "p","d" => "q",
+	# "e" => "r","f" => "s","g" => "t","h" => "u",
+	# "i" => "v","j" => "w","k" => "x","l" => "y",
+	# "m" => "z","n" => "a","o" => "b","p" => "c",
+	# "q" => "d","r" => "e","s" => "f","t" => "g",
+	# "u" => "h","v" => "i","w" => "j","x" => "k",
+	# "y" => "l","z" => "m"," " => "*"}
+	#end
 	
-	def encrypt_letter(letter, rotation)
-		lowercase_letter = letter.downcase
-		cipher[lowercase_letter]
+	def encrypt_letter(letter,rotation)
+		cipher_rotation = cipher(rotation)
+		cipher_rotation[letter]
 	end
 
-	def encrypt(string, rotation)
+	def encrypt(string,rotation)
 		#1 cut the input string into letters
 		letters = string.split("")
 	#2Encrypt those letters one at a time
-		
 		encrypted_letters = letters.collect do |letter|
-		 encrypt_letter(letter)
-	
-	end
+		 encrypt_letter(letter, rotation)
+		end
 
 	#3join the results back together
 		encrypted_letters.join("")
 	end
 
 	def cipher(rotation)
-  characters = (' '..'z').to_a
-  rotated_characters = characters.rotate(rotation)
-  Hash[characters.zip(rotated_characters)]
-end
+  		characters = (' '..'z').to_a
+  		rotated_characters = characters.rotate(rotation)
+  		Hash[characters.zip(rotated_characters)]
+	end
 
+	def encrypt_file(filename, rotation)
 
+    # Create the file handle to the input file
+    input = File.open(filename, "r")
+    # Read the text of the input file
+    text = input.read
+    # Encrypt the text
+    encrypted_text = encrypt(text,rotation)
+    # Create a name for the output file
+    new_name = filename + '.encrypted'
+    # Create an output file handle
+    new_name = File.open(new_name, "w")
+    # Write out the text
+    new_name.write(encrypted_text)
+    # Close the file
+    new_name.close
+		
+	end
+
+	# def crack(string)
+		# cipher(1).count.times do |i|
+			# puts encrypt
+		# end
+	# end
 end
 
